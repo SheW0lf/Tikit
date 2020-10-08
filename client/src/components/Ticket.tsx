fae6f51b82abf350b395e0f00741e6b0729448e5
Amber Cyr <seylovie@gmail.com>
2020-10-07 21:44:31 -0700

    create Ticket skeleton

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+type Label = ['bug', 'ticket'];
+
+interface TicketProps {
+	title: string;
+	tags: Label[] | Label;
+	date: string;
+}
+
+const Ticket = ({ title, tags, date }: TicketProps) => {
+	return <div></div>;
+};
+
+export default Ticket;
