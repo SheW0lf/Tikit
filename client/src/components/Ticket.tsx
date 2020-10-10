0f94a419527fb5bb0d986679caa032fcc00567aa
Tom Shamp <T.shamp7@gmail.com>
2020-10-10 11:37:51 -0700

    Fixed type issues

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-type Props = {
+
+type Label = "bug" | "ticket";
+
+interface Details {
 	title: string;
-	tags: string;
+	tags: Label[];
 	date: string;
 };
 
 
-const Ticket = ({ title, tags, date }: Props) => {
+const Ticket = ({ title, tags, date }: Details) => {
 	return (
 		<div className="box-border p-4 m-5 border-2 border-pink bg-gray-200 rounded">
 			<h3 className="text-green text-start p-2">{title}</h3>
