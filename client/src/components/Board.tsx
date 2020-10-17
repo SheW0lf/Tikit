ef0d556331731711853abfed32c7c44c35330ddc
Amber Cyr <seylovie@gmail.com>
2020-10-17 15:11:49 -0700

    updated board rendering

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import Ticket from './Ticket'
+import Ticket from './Ticket';
 
 export function Board() {
 	type Props = {
 		title: string;
-
+		content?: JSX.Element[];
 	};
-	const Column = ({ title }: Props) => {
+
+	const TicketInfo = (
+		<Ticket
+			title="example title"
+			tags={['bug', 'ticket']}
+			date="todays date"
+		></Ticket>
+	);
+
+	const Column = ({ title, content }: Props) => {
 		return (
 			<div className="bg-dark-blue min-h-full">
-				<h3 className="text-green text-center p-2">{title}</h3>
-				<Ticket title="example title" tags="bug" date="todays date"></Ticket>
+				<h3 className="text-green text-center p-2 text-2xl">{title}</h3>
+				{content}
 			</div>
 		);
 	};
 	return (
 		<div className="grid grid-cols-4 bg-blue gap-6 m-10 p-6 min-h-screen">
-			<Column title="To Do" />
+			<Column
+				title="To Do"
+				content={[TicketInfo, TicketInfo, TicketInfo, TicketInfo]}
+			/>
 			<Column title="In Progress" />
 			<Column title="Needs Review" />
 			<Column title="Done" />
