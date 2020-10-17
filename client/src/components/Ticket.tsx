f77d3c247c2768ad247acb624cb51bf1732a1b73
Amber Cyr <seylovie@gmail.com>
2020-10-17 15:11:03 -0700

    updated styling

--- client/src/components/Ticket.tsx
+++ client/src/components/Ticket.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-type Props = {
+
+type Label = 'bug' | 'ticket';
+
+interface Details {
 	title: string;
-	tags: string;
+	tags: Label[];
 	date: string;
-};
+}
 
-
-const Ticket = ({ title, tags, date }: Props) => {
+const Ticket = ({ title, tags, date }: Details) => {
 	return (
-		<div className="box-border p-4 m-5 border-2 border-pink bg-gray-200 rounded">
-			<h3 className="text-green text-start p-2">{title}</h3>
-			<p className="text-green text-start p-2">{tags}</p>
-			<p className="text-green text-start p-2">{date}</p>
+		<div className="box-border p-4 m-5 bg-blue rounded">
+			<h1 className="text- text-start text-xl text-light-blue">
+				{title}
+			</h1>
+			<p className="text-green text-start pb-2">{date}</p>
+			<div className="flex flex-row">
+				{tags.map((tag) => (
+					<p className="text-pink text-start padding pr-2 py-1">
+						{tag}
+					</p>
+				))}
+			</div>
 		</div>
 	);
 };
